d0838f0b26fbc76b04d2412421fa71e41c7e321e
Andrea Gutierrez <agutierrezmorales17@gmail.com>
2026-01-25 01:28:42 -0600

    shiftsitter-web

---
+++ app/employers/onboarding/page.tsx
@@ -0,0 +1,8 @@
+export default function EmployersOnboardingPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Employers onboarding</h1>
+      <p>Coming soon.</p>
+    </main>
+  );
+}
